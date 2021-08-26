b5fad095871bb8838c72d8219728209d04cfc4d0
Renann Santos <69852026+Renann1@users.noreply.github.com>
2021-08-26 14:58:11 -0300

    Add files via upload

---
+++ chat_bot/bot/chat.php
@@ -0,0 +1,7 @@
+<?php
+include "Bot.php";
+$bot = new Bot;
+$questions = [
+    "o que é php" => "É uma linguagem de programação server side.",
+    "o que é linux" => "É um sistema operacional criado por Linus Torvald."
+];
